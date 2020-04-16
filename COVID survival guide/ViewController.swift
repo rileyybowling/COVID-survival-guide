3b11a26d0f5f5b6b3e6d9b9edd0d2e85f8bdb6ca
Riley Bowling <rbowling@bsd220.org>
2020-04-16 11:16:29 -0500

    Initial Commit

---
+++ COVID survival guide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID survival guide
+//
+//  Created by Riley Bowling on 4/16/20.
+//  Copyright © 2020 Riley Bowling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
