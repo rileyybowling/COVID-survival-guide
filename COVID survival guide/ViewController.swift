102aeb4e0eed787c980bcccb300899bb410c7c64
Riley Bowling <rbowling@bsd220.org>
2020-04-16 11:25:59 -0500

    added user current location

--- COVID survival guide/ViewController.swift
+++ COVID survival guide/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
     }
 
 
